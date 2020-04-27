714de0336ca38b87dfa53f6f0bb5b79d40e1e4f5
Deividas <deividassabaliauskas@gmail.com>
2020-04-27 11:59:36 +0300

    html and css tapk dealer form

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,12 @@
+
+<head>
+<link rel="stylesheet" type="text/css" href="css/app.css">
+</head>
+@extends('layouts.app')
+
+
+@section('content')
+
+@include('forms.pildymo_forma')
+
+@stop 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('views.main');
+    return view('homepage');
 });
