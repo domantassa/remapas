0d22126d4927d70f81b0072d1121bcf874398d40
Deividas <deividassabaliauskas@gmail.com>
2020-05-02 16:26:21 +0300

    fixed favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
   <link rel="stylesheet" href="{{asset('css/app.css')}}">
   <script src="js/app.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="https://www.remapas.eu/images/icon2.png">
+  <link rel="shortcut icon" href="{{asset('images/icon2.png')}}">
   @yield('meta')
 </head>
 
